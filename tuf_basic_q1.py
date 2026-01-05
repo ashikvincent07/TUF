b4c20f8a926f3d04fd1a368b6696309502ac3726
ashik <ashikvincent6@gmail.com>
2026-01-05 10:41:06 +0530

    tuf basic q1 question added

--- tuf_basic_q1.py
+++ tuf_basic_q1.py
@@ -1,3 +1,6 @@
+"""
+Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+"""
 class Solution:
     def printNumber(self):
         number = int(input())
